db9a94ac4e4720078cf0e17a2270743120d21873
Ashish Shukla <ashish_shukla@epam.com>
2026-08-30 19:23:48 +0530

    fix(dashboard): remove error message return on invoice deletion failure

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -120,9 +120,6 @@ export async function deleteInvoice(id: string) {
   `;
   } catch (error) {
     console.error(error);
-    return {
-      message: "Database Error: Failed to Delete Invoice.",
-    };
   }
 
   revalidatePath("/dashboard/invoices");
